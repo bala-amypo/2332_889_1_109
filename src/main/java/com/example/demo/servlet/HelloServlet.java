96ec6d1b3c1bb9baacd06b52441d2bafb848f76a
bala-amypo <bala@amypo.in>
2025-12-21 15:12:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -0,0 +1,8 @@
+package com.example.demo.servlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) { } [cite: 1686]
+}
