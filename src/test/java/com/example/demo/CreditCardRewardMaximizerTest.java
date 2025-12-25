75b734e026aeeccbcdb19eeca050b13651f39eb2
bala-amypo <bala@amypo.in>
2025-12-25 15:13:41 +0000

    User commit

--- src/test/java/com/example/demo/CreditCardRewardMaximizerTest.java
+++
@@ -1,8 +0,0 @@
-// package com.example.demo;
-
-// public class CreditCardRewardMaximizerTest {
-
-//     public static void main(String[] args) {
-//         System.out.println("Application compiled successfully");
-//     }
-// }
